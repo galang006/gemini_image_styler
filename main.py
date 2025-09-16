4016e366af26da219d89168d9c57c19e624af102
Galang Satriamahesa Putra Dewa <galangsatriamahesa.pd@gmail.com>
2025-09-16 12:37:56 +0700

    upload

---
+++ main.py
@@ -0,0 +1,80 @@
+import base64
+import os
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def save_binary_file(file_name, data):
+    f = open(file_name, "wb")
+    f.write(data)
+    f.close()
+
+def generate(img_name):
+    client = genai.Client(
+        api_key = os.getenv("GOOGLE_API_KEY"),
+    )
+
+    files = [
+        
+        client.files.upload(file=f"image/{img_name}.jpg"),
+    ]
+    model = "gemini-2.0-flash-exp-image-generation"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_uri(
+                    file_uri=files[0].uri,
+                    mime_type=files[0].mime_type,
+                ),
+                types.Part.from_text(text="""
+                                     Edit this photo by only changing the clothing. 
+                                     Keep the person exact face, pose, and body unchanged. 
+                                     Do not modify facial features, expression, hairstyle, or proportions. 
+                                     Only replace clothes with a realistic black formal suit.
+                                     """),
+            ],
+        )
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        temperature=0.3,
+        top_p=0.95,
+        top_k=40,
+        max_output_tokens=8192,
+        response_modalities=[
+            "image",
+            "text",
+        ],
+        safety_settings=[
+            types.SafetySetting(
+                category="HARM_CATEGORY_CIVIC_INTEGRITY",
+                threshold="OFF",  
+            ),
+        ],
+        response_mime_type="text/plain",
+    )
+
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        if not chunk.candidates or not chunk.candidates[0].content or not chunk.candidates[0].content.parts:
+            continue
+        if chunk.candidates[0].content.parts[0].inline_data:
+            file_name = f"image/{img_name}_output.jpg"
+            save_binary_file(
+                file_name, chunk.candidates[0].content.parts[0].inline_data.data
+            )
+            print(
+                "File of mime type"
+                f" {chunk.candidates[0].content.parts[0].inline_data.mime_type} saved"
+                f"to: {file_name}"
+            )
+        else:
+            print(chunk.text)
+
+if __name__ == "__main__":
+    generate("DSCF0252")
